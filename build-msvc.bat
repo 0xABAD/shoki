96d3021b4aa3d0f28f4a50497428ba882fde5c5e
Dan Abad <Dan Abad>
2017-05-07 21:06:14 -0500

    Add debug and link information to MSVC build script

--- build-msvc.bat
+++ build-msvc.bat
@@ -2,10 +2,14 @@
 setlocal enableextensions
 
 set SRC=%CD%\src
+set DEBUG=-Od -Zi -Fd:pwThief.pdb
+set RELEASE=-O2
+set TARGET=%DEBUG%
 
 if not exist build (mkdir build)
 pushd build
 
-cl -Od %SRC%\main.cpp -Fe:pwThief.exe
+cl -nologo %TARGET% %SRC%\main.cpp -Fe:pwThief.exe ^
+   -link kernel32.lib gdi32.lib comdlg32.lib user32.lib
 
 popd
