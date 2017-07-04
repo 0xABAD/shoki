1589a648fbc326931eb36ee5860e9e0d31152bc8
Dan Abad <dan.r.abad@gmail.com>
2017-07-04 01:24:57 -0500

    Update build scripts

--- build-gcc.bat
+++ build-gcc.bat
@@ -3,15 +3,15 @@ setlocal enableextensions
 
 set PROJ=%CD%
 set SRC=%PROJ%\src
-set DEBUG=-g
+set DEBUG=-g -DDEBUG
 set RELEASE=-O3
-set TARGET=%DEBUG%
-set RUN_AFTER_BUILD=1
+set TARGET=%RELEASE%
+set RUN_AFTER_BUILD=0
 
 if not exist %PROJ%\build (mkdir %PROJ%\build)
 pushd %PROJ%\build
 
-g++ %TARGET% %SRC%\main.cpp -o shoki.exe -m64 -mwindows -mwin32 -lgdiplus
+g++ %TARGET% %SRC%\main.cpp -Wno-write-strings -o shoki.exe -m64 -mwindows -mwin32 -lgdiplus
 
 if %RUN_AFTER_BUILD%==1 shoki.exe
 
--- build-msvc.bat
+++ build-msvc.bat
@@ -3,16 +3,16 @@ setlocal enableextensions
 
 set PROJ=%CD%
 set SRC=%PROJ%\src
-set DEBUG=-Od -Zi -Fd:shoki.pdb
+set DEBUG=-Od -Zi -Fd:shoki.pdb -DDEBUG
 set RELEASE=-O2
-set TARGET=%DEBUG%
-set RUN_AFTER_BUILD=1
+set TARGET=%RELEASE%
+set RUN_AFTER_BUILD=0
 
 if not exist %PROJ%\build (mkdir %PROJ%\build)
 pushd %PROJ%\build
 
 cl -nologo %TARGET% %SRC%\main.cpp -Fe:shoki.exe ^
-   -link kernel32.lib gdi32.lib gdiplus.lib comdlg32.lib user32.lib
+   -link gdi32.lib gdiplus.lib user32.lib
 
 if %RUN_AFTER_BUILD%==1 shoki.exe
 
